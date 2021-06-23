fa77021ee90e69accd819289082a89553dba6431
Sancler Zanella <sanclerzjj@gmail.com>
2021-06-23 02:46:07 +0000

    Add content to landpage.html; Add login.html and signup.html templates; Add register functionality; Add login functionality; Add log out functionality

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (Flask, flash,
                    request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists('env.py'):
     import env
 
@@ -25,9 +26,76 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+# View for landpage
 @app.route("/")
 def landpage():
     return render_template("landpage.html")
+
+
+# View for Sign Up Page
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        # Check if the username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"email": request.form.get("email")})
+
+        if existing_user:
+            flash("User already exists")
+            return redirect(url_for("signup"))
+
+        register = {
+            "fname": request.form.get("fname").lower(),
+            "lname": request.form.get("lname").lower(),
+            "email": request.form.get("email"),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # Put the new user into 'session' cookie
+        session['user'] = existing_user["fname"].capitalize()
+        flash("Registration Successful")
+        return redirect(url_for("landpage"))
+
+    return render_template("signup.html")
+
+
+# View to execute the login page and form (Read from DB)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Check if the username exists in database
+        existing_user = mongo.db.users.find_one(
+            {"email": request.form.get("email")})
+
+        if existing_user:
+            # Ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = existing_user["fname"].capitalize()
+                    flash("Welcome, {}".format(
+                        existing_user["fname"].capitalize()))
+                    return redirect(url_for("landpage"))
+            else:
+                # Invalid password
+                flash("Incorrect username and/or password")
+                return redirect(url_for("login"))
+
+        else:
+            # Username doesn't exist
+            flash("Incorrect username and/or password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+# View to logout the user (Clear the session cookie)
+@app.route("/logout")
+def logout():
+    # Remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 # Define host and port for the app
