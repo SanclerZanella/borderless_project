15530ddbee483d5e8f16a6afe9e147e3601e1e3d
Sancler Zanella <sanclerzjj@gmail.com>
2021-07-27 00:36:22 +0000

    Delete countries collection from database and creates an array in app.py with countries name, to reduce the database size; Add 'Friends' option to trip privacy; Create 404.html and 500.html and functionality to handle 404 and 500 codes response

--- app.py
+++ app.py
@@ -45,6 +45,267 @@ cloudinary.config(
 map_key = os.environ.get("MAP_KEY")
 
 
+# Handle 500 Internal Server error
+@app.errorhandler(500)
+def error_500(error):
+    return render_template("500.html"), 500
+
+
+# Handle 404 not found error
+@app.errorhandler(404)
+def error_404(error):
+    return render_template("404.html"), 404
+
+
+# List of countries
+def countries():
+    countries = ["Afghanistan",
+                 "land Islands",
+                 "Albania",
+                 "Algeria",
+                 "American Samoa",
+                 "AndorrA",
+                 "Angola",
+                 "Anguilla",
+                 "Antarctica",
+                 "Antigua and Barbuda",
+                 "Argentina",
+                 "Armenia",
+                 "Aruba",
+                 "Australia",
+                 "Austria",
+                 "Azerbaijan",
+                 "Bahamas",
+                 "Bahrain",
+                 "Bangladesh",
+                 "Barbados",
+                 "Belarus",
+                 "Belgium",
+                 "Belize",
+                 "Benin",
+                 "Bermuda",
+                 "Bhutan",
+                 "Bolivia",
+                 "Bosnia and Herzegovina",
+                 "Botswana",
+                 "Bouvet Island",
+                 "Brazil",
+                 "British Indian Ocean Territory",
+                 "Brunei Darussalam",
+                 "Bulgaria",
+                 "Burkina Faso",
+                 "Burundi",
+                 "Cambodia",
+                 "Cameroon",
+                 "Canada",
+                 "Cape Verde",
+                 "Cayman Islands",
+                 "Central African Republic",
+                 "Chad",
+                 "Chile",
+                 "China",
+                 "Christmas Island",
+                 "Cocos (Keeling) Islands",
+                 "Colombia",
+                 "Comoros",
+                 "Congo",
+                 "Congo, The Democratic Republic of the",
+                 "Cook Islands",
+                 "Costa Rica",
+                 "Cote D'Ivoire",
+                 "Croatia",
+                 "Cuba",
+                 "Cyprus",
+                 "Czech Republic",
+                 "Denmark",
+                 "Djibouti",
+                 "Dominica",
+                 "Dominican Republic",
+                 "Ecuador",
+                 "Egypt",
+                 "El Salvador",
+                 "Equatorial Guinea",
+                 "Eritrea",
+                 "Estonia",
+                 "Ethiopia",
+                 "Falkland Islands (Malvinas)",
+                 "Faroe Islands",
+                 "Fiji",
+                 "Finland",
+                 "France",
+                 "French Guiana",
+                 "French Polynesia",
+                 "French Southern Territories",
+                 "Gabon",
+                 "Gambia",
+                 "Georgia",
+                 "Germany",
+                 "Ghana",
+                 "Gibraltar",
+                 "Greece",
+                 "Greenland",
+                 "Grenada",
+                 "Guadeloupe",
+                 "Guam",
+                 "Guatemala",
+                 "Guernsey",
+                 "Guinea",
+                 "Guinea-Bissau",
+                 "Guyana",
+                 "Haiti",
+                 "Heard Island and Mcdonald Islands",
+                 "Holy See (Vatican City State)",
+                 "Honduras",
+                 "Hong Kong",
+                 "Hungary",
+                 "Iceland",
+                 "India",
+                 "Indonesia",
+                 "Iran, Islamic Republic Of",
+                 "Iraq",
+                 "Ireland",
+                 "Isle of Man",
+                 "Israel",
+                 "Italy",
+                 "Jamaica",
+                 "Japan",
+                 "Jersey",
+                 "Jordan",
+                 "Kazakhstan",
+                 "Kenya",
+                 "Kiribati",
+                 "Korea, Democratic People'S Republic of",
+                 "Korea, Republic of",
+                 "Kuwait",
+                 "Kyrgyzstan",
+                 "Lao People'S Democratic Republic",
+                 "Latvia",
+                 "Lebanon",
+                 "Lesotho",
+                 "Liberia",
+                 "Libyan Arab Jamahiriya",
+                 "Liechtenstein",
+                 "Lithuania",
+                 "Luxembourg",
+                 "Macao",
+                 "Macedonia, The Former Yugoslav Republic of",
+                 "Madagascar",
+                 "Malawi",
+                 "Malaysia",
+                 "Maldives",
+                 "Mali",
+                 "Malta",
+                 "Marshall Islands",
+                 "Martinique",
+                 "Mauritania",
+                 "Mauritius",
+                 "Mayotte",
+                 "Mexico",
+                 "Micronesia, Federated States of",
+                 "Moldova, Republic of",
+                 "Monaco",
+                 "Mongolia",
+                 "Montenegro",
+                 "Montserrat",
+                 "Morocco",
+                 "Mozambique",
+                 "Myanmar",
+                 "Namibia",
+                 "Nauru",
+                 "Nepal",
+                 "Netherlands",
+                 "Netherlands Antilles",
+                 "New Caledonia",
+                 "New Zealand",
+                 "Nicaragua",
+                 "Niger",
+                 "Nigeria",
+                 "Niue",
+                 "Norfolk Island",
+                 "Northern Mariana Islands",
+                 "Norway",
+                 "Oman",
+                 "Pakistan",
+                 "Palau",
+                 "Palestinian Territory, Occupied",
+                 "Panama",
+                 "Papua New Guinea",
+                 "Paraguay",
+                 "Peru",
+                 "Philippines",
+                 "Pitcairn",
+                 "Poland",
+                 "Portugal",
+                 "Puerto Rico",
+                 "Qatar",
+                 "Reunion",
+                 "Romania",
+                 "Russian Federation",
+                 "RWANDA",
+                 "Saint Helena",
+                 "Saint Kitts and Nevis",
+                 "Saint Lucia",
+                 "Saint Pierre and Miquelon",
+                 "Saint Vincent and the Grenadines",
+                 "Samoa",
+                 "San Marino",
+                 "Sao Tome and Principe",
+                 "Saudi Arabia",
+                 "Senegal",
+                 "Serbia",
+                 "Seychelles",
+                 "Sierra Leone",
+                 "Singapore",
+                 "Slovakia",
+                 "Slovenia",
+                 "Solomon Islands",
+                 "Somalia",
+                 "South Africa",
+                 "South Georgia and the South Sandwich Islands",
+                 "Spain",
+                 "Sri Lanka",
+                 "Sudan",
+                 "Suriname",
+                 "Svalbard and Jan Mayen",
+                 "Swaziland",
+                 "Sweden",
+                 "Switzerland",
+                 "Syrian Arab Republic",
+                 "Taiwan",
+                 "Tajikistan",
+                 "Tanzania, United Republic of",
+                 "Thailand",
+                 "Timor-Leste",
+                 "Togo",
+                 "Tokelau",
+                 "Tonga",
+                 "Trinidad and Tobago",
+                 "Tunisia",
+                 "Turkey",
+                 "Turkmenistan",
+                 "Turks and Caicos Islands",
+                 "Tuvalu",
+                 "Uganda",
+                 "Ukraine",
+                 "United Arab Emirates",
+                 "United Kingdom",
+                 "United States",
+                 "United States Minor Outlying Islands",
+                 "Uruguay",
+                 "Uzbekistan",
+                 "Vanuatu",
+                 "Venezuela",
+                 "Viet Nam",
+                 "Virgin Islands, British",
+                 "Virgin Islands, U.S.",
+                 "Wallis and Futuna",
+                 "Western Sahara",
+                 "Yemen",
+                 "Zambia",
+                 "Zimbabwe"]
+    return countries
+
+
 # Get image url from cloud
 def img_url(folder, filename):
     pic_url = cloudinary.CloudinaryImage('%s/%s' % (folder,
@@ -565,9 +826,6 @@ def profile():
                                                     folder_name)
             cloudinary.api.create_folder(folder_path)
 
-    # List of countries
-    countries = list(mongo.db.countries.find())
-
     # Pagination
     db = mongo.db.trips
     db_field = 'user'
@@ -627,7 +885,7 @@ def profile():
     notifications = current_user['notifications']
 
     return render_template('profile.html',
-                           countries=countries,
+                           countries=countries(),
                            trips=trips_pag,
                            full_name=full_name,
                            current_user_id=current_user_id,
@@ -649,6 +907,7 @@ def profile():
 # Public profile view
 @ app.route('/public_profile/<trip_user>', methods=["GET", "POST"])
 def public_profile(trip_user):
+
     user = mongo.db.users.find_one({'_id': ObjectId(trip_user)})
 
     # Pagination
@@ -789,6 +1048,7 @@ def likes(trip_id):
 # View to edit profile
 @ app.route('/edit_profile', methods=["POST", "GET"])
 def edit_profile():
+
     current_user = mongo.db.users.find_one({'_id': ObjectId(session['user'])})
     current_user_id = current_user['_id']
 
@@ -828,6 +1088,7 @@ def edit_profile():
 # View to edit a trip
 @ app.route('/edit_trip/<trip_id>', methods=['GET', 'POST'])
 def edit_trip(trip_id):
+
     current_trip = mongo.db.trips.find_one({'_id': ObjectId(trip_id)})
     current_catg = current_trip['trip_category'].lower()
     current_name = current_trip['trip_name'].lower()
@@ -927,9 +1188,6 @@ def edit_trip(trip_id):
                                   multi=True)
             return redirect(url_for('profile'))
 
-    # List of countries
-    countries = list(mongo.db.countries.find())
-
     current_user_id = session['user']
     current_user = mongo.db.users.find_one(
         {'_id': ObjectId(current_user_id)})
@@ -939,7 +1197,7 @@ def edit_trip(trip_id):
                            profile_pic=get_profile_pic,
                            current_user=current_user,
                            current_trip=current_trip,
-                           countries=countries,
+                           countries=countries(),
                            trip_path=trip_path,
                            img_url=img_url,
                            notifications=notifications)
@@ -1000,6 +1258,7 @@ def delete_trip(trip_id):
 # View to open the trip post
 @app.route('/trip/<trip_id>', methods=["GET", "POST"])
 def trip(trip_id):
+
     trip = mongo.db.trips.find_one({'_id': ObjectId(trip_id)})
     resources = trip_folder_resources(trip['trip_name'])
 
