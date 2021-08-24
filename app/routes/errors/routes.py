42aae1353e875c362529746c99a15b7517f82485
Sancler Zanella <sanclerzjj@gmail.com>
2021-08-24 21:05:10 +0000

    Fix errors handler route to work with blueprints, bringing the errors templates(404.htm and 500.html) to the template folder and changing the decorator from @errors.errorhandler() to @errors.app_errorhandler(); Fix pagination to not be shown when the user there is not any trip post

--- app/routes/errors/routes.py
+++ app/routes/errors/routes.py
@@ -10,19 +10,19 @@ App routes
 """
 
 
-@errors.errorhandler(500)
+@errors.app_errorhandler(500)
 def error_500(error):
     """
     Handle 500 Internal Server error
     """
 
-    return render_template("erros/500.html"), 500
+    return render_template("500.html"), 500
 
 
-@errors.errorhandler(404)
+@errors.app_errorhandler(404)
 def error_404(error):
     """
     Handle 404 not found error
     """
 
-    return render_template("erros/404.html"), 404
+    return render_template("404.html"), 404
